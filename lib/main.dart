57e0bcc4c8d1e59fd752909d17b32a4f2f50089b
xziztt <xziztt@users.noreply.github.com>
2019-10-27 10:42:38 +0530

    Initial commit
    Base for flutter apps

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(lol());
+}
+  class lol extends StatelessWidget{
+    Widget build(BuildContext context){
+      return MaterialApp(home: Text('hello'));
+    }
+  }

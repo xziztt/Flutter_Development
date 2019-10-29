f4e7441993ddae985e7c65d318265c776d5e7487
xziztt <xziztt@users.noreply.github.com>
2019-10-29 21:00:17 +0530

    Update main.dart
    ->Added Buttons and questions

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,46 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(BaseApp());
-}
-
+void main() => runApp(BaseApp());
+var questionIndex = 0;
 class BaseApp extends StatelessWidget {
-    questions = ['which is your favorite color ?'];
-    Widget build(BuildContext context) {
+  void questionAnswered() {
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
+  }
+
+  var questions = [
+    'which is your favorite color ?',
+    'what is your favorite animal'
+  ];
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('welcome to my app'),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('QUIZZING APP LOL'),
+      ),
+      body: Column(
+        children: [
+          Text(
+            questions[questionIndex],
           ),
-          body: Column(children: [],)),
-    );
+          RaisedButton(
+            child: Text("BLUE"),
+            onPressed: () => print("BLUE selected"),
+          ),
+          RaisedButton(
+            child: Text(
+              "BLACK",
+            ),
+            onPressed: questionAnswered,
+          ),
+          RaisedButton(
+            child: Text("RED"),
+            onPressed: () {
+              print("RED chosen");
+            },
+          ),
+        ],
+      ),
+    ));
   }
 }
